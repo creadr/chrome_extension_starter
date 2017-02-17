79ac9bd81a87d5a37f0b43902e8185dbce13a8d9
Zhuang Liang <zhuang@uber.com>
2017-02-16 21:53:16 -0800

    pass the user page url to the server as a url parameter

--- background.js
+++ background.js
@@ -2,7 +2,14 @@
 
 chrome.browserAction.onClicked.addListener(jumpToUrl);
 function jumpToUrl(tab) {
-    chrome.tabs.update({
-         url: "http://www.example.com/"
-    })
+    chrome.tabs.query({
+        active: true,
+        currentWindow: true
+    }, function(tabs) {
+        console.log(tabs[0].url);
+        chrome.tabs.update({
+             url: "http://localhost:8000?page=" +
+             encodeURIComponent(tabs[0].url)
+        })
+    });
 }
